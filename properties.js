87d6f78a24027d6ccb1b89d7a35d7e4bee52f760
Eric Butz <47221604+ericbutz@users.noreply.github.com>
2022-04-06 16:18:11 -0600

    Delete properties.js

--- properties.js
+++
@@ -1,9 +0,0 @@
-const properties = {
-  server: 'https://fiotestnet.greymass.com',
-  publicKey: 'PutFIOPublicKeyHere',
-  privateKey: 'PutFIOPrivateKeyHere',
-  account: 'PutFIOAccountHere'
-};
-
-
-module.exports = properties;
