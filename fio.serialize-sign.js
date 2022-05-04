1045f1635582d64e4279fee5a10ced9b2c33e21c
Eric Butz <ericgbutz@gmail.com>
2022-05-04 14:36:38 -0600

    Add serilize-sign test for offline signing

---
+++ fio.serialize-sign.js
@@ -0,0 +1,58 @@
+const {FIOSDK } = require('@fioprotocol/fiosdk')
+fetch = require('node-fetch')
+const properties = require('./properties.js')
+
+const fetchJson = async (uri, opts = {}) => {
+  return fetch(uri, opts)
+}
+
+const baseUrl = properties.server + '/v1/'
+
+const privateKey = properties.privateKey,
+  publicKey = properties.publicKey,
+  payeeKey = 'FIO6RkzYHuxaFt7YbyAGqEFFeFCoy2PJD1tZjzymghJk2z9gH2LVY',  // FIO Public Key of the payee
+  amount = 1000000000,
+  max_fee = 100000000000
+
+
+const main = async () => {
+
+    user = new FIOSDK(
+        privateKey,
+        publicKey,
+        baseUrl,
+        fetchJson
+    )
+
+    const chainData = await user.transactions.getChainDataForTx();
+    const transaction = await user.transactions.createRawTransaction({
+        action: 'trnsfiopubky',
+        account: 'fio.token',
+        data: {
+            payee_public_key: payeeKey,
+            amount: amount,
+            max_fee: max_fee,
+            tpid: ''
+        },
+        publicKey,
+        chainData,
+    });
+
+    const { serializedContextFreeData, serializedTransaction } = await user.transactions.serialize({
+        chainId: chainData.chain_id,
+        transaction,
+    });
+
+    const signedTransaction = await user.transactions.sign({
+        chainId: chainData.chain_id,
+        privateKeys: [privateKey],
+        transaction,
+        serializedTransaction,
+        serializedContextFreeData,
+    });
+
+    const result = await user.executePreparedTrx('transfer_tokens_pub_key', signedTransaction);
+    console.log(result);
+}
+
+main();
